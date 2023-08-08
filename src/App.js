747da7402b99f7e4ca5a7449847841c4058ea6f0
trapasonyan <trapasonyan@gmail.com>
2023-08-08 09:55:25 -0300

    atualizado

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef }  from 'react';
+import ConfirmationPage from './ConfirmationPage';
+import banner1 from './imagens/banner1.jpg';
+import banner2 from './imagens/banner2.jpg';
+import banner3 from './imagens/banner3.jpg';
+import banner4 from './imagens/banner4.jpg';
+import banner5 from './imagens/banner5.jpg';
 
-function App() {
+
+const App = () => {
+  const cadastroRef = useRef(null);
+
+  const handleScrollToCadastro = () => {
+    cadastroRef.current.scrollIntoView({behavior: 'smooth'});
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <div className="banner">
+      <img src={banner1} alt='banner1'/>
+      </div>
+
+      <div className="banner">
+      <img src={banner2} alt='banner2'/>
+      </div>
+
+      <div className="banner">
+      <img src={banner3} alt='banner3'/>
+      <div className="arrow" onClick={handleScrollToCadastro}>
+        🠗
+      </div>
+      </div>
+
+      <div className="cadastro-section" ref={cadastroRef}>
+      <ConfirmationPage />
+      </div>
+
+      <div className="banner">
+      <img src={banner4} alt='banner4'/>
+      </div>
+
+      <div className="banner">
+      <img src={banner5} alt='banner5'/>
+      </div>
+
+      </div>
   );
-}
+};
 
 export default App;
+
+
---
+++ src/ConfirmationPage.js
@@ -0,0 +1,113 @@
+import React, { useState } from 'react';
+import './ConfirmationPage.css'
+
+const ConfirmationPage = () => {
+  const [formData, setFormData] = useState({
+    nome: '',
+    nomeEmpresa: '',
+    telefone: '',
+    email: '',
+    documento: '',
+    autorização: false,
+  });
+
+  const handleChange = (e) => {
+    const { name, value, type, checked } = e.target;
+    const newValue = type === 'checkbox' ? checked : value;
+    setFormData({ ...formData, [name]: newValue });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Dados do formulário:', formData);
+    setShowSuccessMessage(true); 
+  };
+
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+
+  return (
+    <div className="container">
+    
+      <h1>Faça seu cadastro aqui:</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Nome:
+          <input
+            type="text"
+            name="nome"
+            value={formData.nome}
+            onChange={handleChange}
+            required
+          />
+        </label>
+
+        <label>
+          Nome da Empresa:
+          <input
+            type="text"
+            name="nomeEmpresa"
+            value={formData.nomeEmpresa}
+            onChange={handleChange}
+            required
+          />
+        </label>
+
+        <label>
+          Telefone:
+          <input
+            type="tel"
+            name="telefone"
+            value={formData.telefone}
+            onChange={handleChange}
+            required
+          />
+        </label>
+
+        <label>
+          E-mail:
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+        </label>
+
+        <label>
+          CPF/CNPJ:
+          <input
+            type="text"
+            name="documento"
+            value={formData.documento}
+            onChange={handleChange}
+            required
+          />
+        </label>
+
+        <label>
+          *Autorizo o recebimento de notificações no WhatsApp, SMS e e-mails sobre promoções e novidades do Mega Atacado:
+          <input
+            type="checkbox"
+            name="autorização"
+            checked={formData.autorização}
+            onChange={handleChange}
+          />
+        </label>
+
+        <button type="submit" className="btn">Confirmar Cadastro</button>
+      </form>
+      {showSuccessMessage && (
+        <div className="success-message">
+          Cadastro realizado com sucesso!
+          <button onClick={() => setShowSuccessMessage(false)}>Fechar</button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ConfirmationPage;
+
+
+
